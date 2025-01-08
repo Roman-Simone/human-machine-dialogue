b59d24f7297eaa4d972c2f2bd2e681e4b3d40e01
Roman Simone <83060406+Roman-Simone@users.noreply.github.com>
2025-01-08 14:32:17 +0100

    add package

---
+++ chat.py
@@ -0,0 +1,19 @@
+import omegaconf
+
+
+class Chat():
+    def __init__(self):
+        self.messages = []
+
+    def add_message(self, message):
+        self.messages.append(message)
+
+    def get_messages(self):
+        return self
+    
+
+
+
+
+if __name__ == "__main__":
+    conf = omegaconf.OmegaConf.load('config.yaml')
