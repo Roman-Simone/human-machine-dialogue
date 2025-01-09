0da09e48feec803267783e56b651170039e078b1
Roman Simone <83060406+Roman-Simone@users.noreply.github.com>
2025-01-09 14:05:16 +0100

    create class NLG

--- components/nlg.py
+++ components/nlg.py
@@ -1,4 +1,34 @@
+import ollama
+from utils.history import History
+
 class NLG():
 
-    def __init__(self):
-        pass
+    def __init__(self, model, prompt_path):
+        
+        self.model = model
+        self.prompt_path = prompt_path
+        self.history = History()
+    
+
+    def __call__(self, nlu_input = " "):
+        return self.query_model(nlu_input)
+        
+    def query_model(self, nlu_input: str):
+
+        system= open(self.prompt_path, 'r').read()
+        
+        messages = [{
+            'role': 'system',
+            'content': system
+        }] + self.history.get_history()
+
+        messages.append({
+            'role': 'system',
+            'content': nlu_input
+        })
+
+        self.history.add('user', nlu_input)
+
+        response = ollama.chat(model=self.model, messages=messages)
+
+        return response['message']['content']
