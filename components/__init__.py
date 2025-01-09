c3b8a2864194572d263c9ea35c0c4dd541aab6b9
Roman Simone <83060406+Roman-Simone@users.noreply.github.com>
2025-01-09 14:33:07 +0100

    create pre NLU

--- components/__init__.py
+++ components/__init__.py
@@ -1,4 +1,4 @@
-
+from .pre_nlu import PreNLU
 from .nlu import NLU
 from .nlg import NLG
 from .dm import DM
---
+++ components/pre_nlu.py
@@ -0,0 +1,34 @@
+import ollama
+from utils.history import History
+
+
+class PreNLU():
+    def __init__(self, model, prompt_path):
+        
+        self.model = model
+        self.prompt_path = prompt_path
+        self.history = History()
+
+    def __call__(self, user_input = " "):
+        return self.query_model(user_input)
+    
+    def query_model(self, user_input: str):
+        
+        system= open(self.prompt_path, 'r').read()
+        
+        messages = [{
+            'role': 'system',
+            'content': system
+        }] + self.history.get_history()
+
+        messages.append({
+            'role': 'user',
+            'content': user_input
+        })
+
+        self.history.add('user', user_input)
+
+        response = ollama.chat(model=self.model, messages=messages)
+
+        return response['message']['content']
+    
